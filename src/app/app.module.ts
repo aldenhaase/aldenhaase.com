5c7512a7788fe6990b232929db995b83cc9dc9a2
aldenhaase <alden@DESKTOP-43HF1M7.localdomain>
2023-02-16 16:02:16 -0700

    work on lystr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { AboutComponent } from './about/about.component';
 import { NavComponent } from './nav/nav.component';
 import { RunwayComponent } from './runway/runway.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 @NgModule({
@@ -29,7 +30,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FontAwesomeModule,
   ],
   exports: [
   ],
--- src/app/lystr/lystr.component.ts
+++ src/app/lystr/lystr.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faAngleDown, faCloud } from '@fortawesome/free-solid-svg-icons';
+import { faAngular, faDocker, faGolang, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 @Component({
   selector: 'app-lystr',
@@ -8,7 +10,13 @@ import { Component, OnInit } from '@angular/core';
 export class LystrComponent implements OnInit {
 
   constructor() { }
-
+  faAngleDown = faAngleDown;
+  faAngular = faAngular;
+  faDocker = faDocker;
+  faGolang = faGolang;
+  faCloud = faCloud;
+  faGitHub = faGithub;
+  menuActivated = false;
   ngOnInit(): void {
   }
 
