d53977c5a7939b607294bfc2926b6d043fd5f3fc
aldenhaase <aldenhaase@gmail.com>
2023-05-23 11:00:55 -0600

    removed dropdowns fixed missing photo

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,7 +11,7 @@ export class HomeComponent implements OnInit {
   menuItems: links[] = [
       {route: "lystr",    displayName: "- LYSTR -",        type: "project"},
       {route: "keyboard", displayName: "- USB KEYBOARD -", type: "project"},
-      {route: "drums",    displayName: "- DRUMS -", type: "project"},
+      {route: "drums",    displayName: "- MIDI DRUMS -", type: "project"},
       {route: "map",      displayName: "- MAPS FOR BIRDS -", type: "project"},
       {route: "",         displayName: "",                 type: "disabled"},
       {route: "",         displayName: "",                 type: "disabled"},
